8fffa69ccc10a6cba6cc497b84afe011782e8dae
your_username <mcevoyclark@gmail.com>
2022-12-31 11:05:03 +0100

    changes

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/vitalcool-server";
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/vitalcool-server";
 // const MONGO_URI =
 //   "mongodb+srv://mcevoyclark:VNYeLZE3U04xKg3A@cluster0.vsj653m.mongodb.net/Vitalcool-server?retryWrites=true&w=majority";
 
